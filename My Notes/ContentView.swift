1bf560e6771323b6f5b10c19829007f41b09f855
Ankit kumar <iamankitm05@gmail.com>
2026-03-08 21:20:29 +0530

    Initial Commit

---
+++ My Notes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  My Notes
+//
+//  Created by Ankit Kumar Ojha on 08/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My Notes/My_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_NotesApp.swift
+//  My Notes
+//
+//  Created by Ankit Kumar Ojha on 08/03/26.
+//
+
+import SwiftUI
+
+@main
+struct My_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
